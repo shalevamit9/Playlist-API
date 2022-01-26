c98575cf7aafef9d493ba9b4e1986bbce036cb29
Amit Shalev <amits@rapyd.ac>
2022-01-26 14:26:17 +0200

    remove comment

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -6,20 +6,6 @@ import { UnauthorizedException } from '../exceptions/unauthorized.exception.js';
 
 const { APP_SECRET } = process.env;
 
-// export const verifyAuth: RequestHandler = async (req, res, next) => {
-//   const accessToken = req.headers['x-access-token'] as string | undefined;
-//   if (!accessToken) throw new UnauthorizedException();
-
-//   try {
-//     const { userId } = await jwt.verify(accessToken, APP_SECRET as string);
-//     req.userId = userId;
-
-//     next();
-//   } catch (error) {
-//     throw new UnauthorizedException();
-//   }
-// };
-
 export const verifyAuth = (roles: string[]): RequestHandler => {
   return (req, res, next) => {
     const accessToken = req.headers['x-access-token'] as string | undefined;
